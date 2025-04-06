f3a1b17c70f08048ffdbf33b3fba7a5fc9051443
Shubh1619 <ubalekartik203@gmail.com>
2025-04-06 23:00:55 +0530

    add put delete

--- app/routers/items.py
+++ app/routers/items.py
@@ -12,7 +12,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-@router.post("/post", response_model=ItemResponse)
+@router.post("/create_user", response_model=ItemResponse)
 def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     db_item = Item(name=item.name,email=item.email, password=item.password)
     db.add(db_item)
@@ -20,14 +20,14 @@ def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     db.refresh(db_item)
     return db_item
 
-@router.get("/{item_id}", response_model=ItemResponse)
+@router.get("/get_user/{item_id}", response_model=ItemResponse)
 def read_item(item_id: int, db: Session = Depends(get_db)):
     db_item = db.query(Item).filter(Item.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return db_item
 
-@router.put("/{item_id}", response_model=ItemResponse)
+@router.put("/update_user/{item_id}", response_model=ItemResponse)
 def update_item(item_id: int, item: ItemCreate, db: Session = Depends(get_db)):
     db_item = db.query(Item).filter(Item.id == item_id).first()
     if db_item is None:
@@ -39,7 +39,7 @@ def update_item(item_id: int, item: ItemCreate, db: Session = Depends(get_db)):
     db.refresh(db_item)
     return db_item
 
-@router.delete("/{item_id}", response_model=ItemResponse)
+@router.delete("/delete_user/{item_id}", response_model=ItemResponse)
 def delete_item(item_id: int, db: Session = Depends(get_db)):
     db_item = db.query(Item).filter(Item.id == item_id).first()
     if db_item is None:
